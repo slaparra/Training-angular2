7d7189314f76a2ab80d9be94dad8e0b19d5b7296
slaparra <santiago.laparra@atrapalo.com>
2016-07-23 22:34:29 +0200

    any route not defined open AtrapaloTeamComponent

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,8 +3,9 @@ import { AtrapaloTeamComponent } from './atrapalo-team/atrapalo-team.component';
 import { UserProfileComponent } from './user/user-profile.component';
 
 const routes: RouterConfig = [
-    { path: '', component: AtrapaloTeamComponent },
-    { path: 'user-profile/:id', component: UserProfileComponent }
+    { path: '', component: AtrapaloTeamComponent},
+    { path: 'user-profile/:id', component: UserProfileComponent },
+    { path: '**', component: AtrapaloTeamComponent }
 ];
 
 export const appRouterProviders = [
